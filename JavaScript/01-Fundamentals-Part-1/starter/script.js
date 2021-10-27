a307535bc84cbab91ad721b4d2783a0ef16a8d03
Rohan <rohanmane@benchmarkit.solutions>
2021-10-27 08:00:42 +0530

    section 1 - part 1 - 1

---
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+let js = 'Amezing';
+// if(js=== 'Amezing') alert("JavaScript is Great");
+
+ console.log(40+8+23-11);
+
+ console.log("Rohan");
+ console.log(44);
+
+ let firstName = "Ronny";
+ console.log(firstName);
+ 
+ //==================================================================
+
+ // Assignment - vslues & variables
+ let country = "India";
+ let continent = "Asia";
+ let population = 1.38
+
+ console.log(country);
+ console.log(continent);
+ console.log(population + " billion");
+
+ //===============================================================
+//Assignment - Data Type
+let isIsland = false;
+let language = "Hindi";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
