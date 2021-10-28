5d98502bccbee13f61682e69c9171725af40c355
rohan <rohanmane2929r@gmail.com>
2021-10-28 08:50:45 +0530

    ch2

--- JavaScript/01-Fundamentals-Part-1/codingChalleges/ch1.js
+++ JavaScript/01-Fundamentals-Part-1/codingChalleges/ch1.js
@@ -1,4 +1,4 @@
-// Mark and Jhon
+// Mark and Jhon 
 let markWeight = 78;
 let markHeight = 1.69;
 let jhonWeight = 92;
@@ -9,3 +9,21 @@ let bmiJhon = jhonWeight/(jhonHeight**2);
 
 let markHigherBmi = bmiMark > bmiJhon;
 console.log(markHigherBmi);
+
+//ch2
+
+// if (bmiMark > bmiJhon)
+// {
+//     console.log("Mark's BMI is higher than Jhon's");
+// }
+// else{
+//     console.log("Jhon's BMI is higher than Mark's");
+// }
+
+if (bmiMark > bmiJhon)
+{
+    console.log(`Mark's BMI${(bmiMark)} is higher than Jhon's${(bmiJhon)}`);
+}
+else{
+    console.log(`Jhon's BMI${bmiJhon} is higher than Mark's${bmiMark}`);
+}
