96bf88191bda2af840a81bf0d6a84a1060b39e56
rohan <rohanmane2929r@gmail.com>
2021-11-07 17:07:50 +0530

    Assignment and sec 9

---
+++ JavaScript/JS Individual assignment - CRUD/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const blogInput = document.querySelector(".blog-input");
+const blogButton = document.querySelector(".blog-button");
+const blogList = document.querySelector(".blog-list");
+
+
