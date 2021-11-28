eef9019ddef30dee8b25a4e75908b3df73b36f82
Rohan <rohanmane@benchmarkit.solutions>
2021-11-29 00:06:00 +0530

    crud with mvc

---
+++ MVC_oops_Crud/views/home/index.php
@@ -0,0 +1,48 @@
+<div class="col-lg" style="padding-top: 5%">
+        <div>
+           <a class="btn btn-primary" href="<?php echo ROOT_PATH; ?>store/add/"
+">Add Book</a>
+           <ul class="nav navbar-nav navbar-right ">
+                <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+                <input type="search" name="search_data">
+                <input type="submit" name="search" class="btn btn-info" value="Search">
+                </form>
+            </ul>
+        </div>
+        <div>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr class="bg-dark text-white text-center">
+                    <th>Sr. No.</th>
+                    <th><a href="?sort=isbn">^</a> ISBN</th>
+                    <th><a href="?sort=bookname">^</a>Book Name</th>
+                    <th>Author Name</th>
+                    <th>Publication</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $sr = 1;?>
+                <?php foreach ($viewmodel as $value) : ?>
+                    
+                <tr class="text-center">
+                    <td><?php echo $sr; ?></td>
+                    <td><?php echo $value['isbn']; ?></td>
+                    <td><?php echo $value['bookname']; ?></td>
+                    <td><?php echo $value['authorname']; ?></td>
+                    <td><?php echo $value['bpublication']; ?></td>
+                    <td><?php echo $value['price']; ?></td>
+                    <td>
+                        <a class="btn btn-info" href="<?php echo ROOT_URL;?>store/add/<?php echo $value['id'];?>">🖊</a>
+                         <a class="btn btn-danger" href="<?php echo ROOT_URL;?>store/delete/<?php echo $value['id'];?>">🗑</a>
+                    </td>
+                </tr>
+                <?php $sr++;?>
+                <?php endforeach; ?>
+
+             
+            </tbody>
+        </table>
+    </div>
+</div>
